3ae9e156317b1c4902d65bee84d51b9031121896
alif_ramadhan <alif@skinmystery.com>
2022-09-29 19:27:58 +0700

    initial commit

---
+++ main.py
@@ -0,0 +1,102 @@
+import time 
+import numpy as np
+from tqdm import tqdm
+from tensorflow.keras.optimizers import Adam
+from sklearn.metrics import accuracy_score, confusion_matrix, classification_report
+
+from utils import (
+    ROOT, 
+    split_dataset, 
+    create_triplets, 
+    get_siamese_network, 
+    SiameseModel, 
+    get_batch, 
+    test_on_triplets,
+    extract_encoder,
+    classify_images,
+)
+
+def run():
+    train_list, test_list = split_dataset(ROOT, split = 0.9)
+    print("Length of training list:", len(train_list))
+    print("Length of testing list :", len(test_list))
+
+    # train_list, test list contains the folder names along with the number of files in the folder.
+    print("\nTest List:", test_list)
+
+    train_triplet = create_triplets(ROOT, train_list)
+    test_triplet  = create_triplets(ROOT, test_list)
+
+    print("Number of training triplets:", len(train_triplet))
+    print("Number of testing triplets :", len(test_triplet))
+
+    print("\nExamples of triplets:")
+    for i in range(5):
+        print(train_triplet[i])
+
+    siamese_network = get_siamese_network()
+    print(siamese_network.summary())
+
+    siamese_model = SiameseModel(siamese_network)
+    optimizer = Adam(learning_rate=1e-3, epsilon=1e-01)
+    siamese_model.compile(optimizer=optimizer)
+
+    save_all = False
+    epochs = 256
+    batch_size = 128
+
+    max_acc = 0
+    train_loss = []
+    test_metrics = []
+
+    for epoch in tqdm(range(1, epochs+1), desc = "Epoch"):
+        t = time.time()
+        
+        # Training the model on train data
+        epoch_loss = []
+        for data in get_batch(train_triplet, batch_size=batch_size):
+            loss = siamese_model.train_on_batch(data)
+            epoch_loss.append(loss)
+        epoch_loss = sum(epoch_loss)/len(epoch_loss)
+        train_loss.append(epoch_loss)
+
+        print(f"\nEPOCH: {epoch} \t (Epoch done in {int(time.time()-t)} sec)")
+        print(f"Loss on train    = {epoch_loss:.5f}")
+        
+        # Testing the model on test data
+        metric = test_on_triplets(batch_size=batch_size)
+        test_metrics.append(metric)
+        accuracy = metric[0]
+        
+        # Saving the model weights
+        if save_all or accuracy>=max_acc:
+            siamese_model.save_weights("siamese_model")
+            max_acc = accuracy
+        
+    # Saving the model after all epochs run
+    siamese_model.save("siamese_model_all.h5")
+    siamese_model.save_weights("siamese_model-final")
+
+    # Save the model of encoder extractor
+    encoder = extract_encoder(siamese_model)
+    encoder.save("encoder_all.h5")
+    encoder.save_weights("encoder")
+    print(encoder.summary())
+
+    pos_list = np.array([])
+    neg_list = np.array([])
+
+    for data in get_batch(test_triplet, batch_size=256):
+        a, p, n = data
+        pos_list = np.append(pos_list, classify_images(a, p))
+        neg_list = np.append(neg_list, classify_images(a, n))
+        break
+
+    # Compute and print the accuracy
+    true = np.array([0]*len(pos_list)+[1]*len(neg_list))
+    pred = np.append(pos_list, neg_list)
+    
+    print(f"\nAccuracy of model: {accuracy_score(true, pred)}")
+
+if __name__ == "__main__":
+    run()
---
+++ utils.py
@@ -0,0 +1,297 @@
+import numpy as np
+import os, cv2, random
+
+import tensorflow as tf
+from tensorflow.keras.applications import Xception
+from tensorflow.keras import layers, metrics
+from tensorflow.keras.models import Model, Sequential
+from tensorflow.keras.applications.inception_v3 import preprocess_input
+# Setting random seeds to enable consistency while testing.
+random.seed(5)
+np.random.seed(5)
+tf.random.set_seed(5)
+
+ROOT = "Extracted Faces"
+
+def read_image(index):
+    path = os.path.join(ROOT, index[0], index[1])
+    image = cv2.imread(path)
+    image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+    return image
+
+def split_dataset(directory, split=0.9):
+    folders = os.listdir(directory)
+    num_train = int(len(folders)*split)
+    
+    random.shuffle(folders)
+    
+    train_list, test_list = {}, {}
+    
+    # Creating Train-list
+    for folder in folders[:num_train]:
+        num_files = len(os.listdir(os.path.join(directory, folder)))
+        train_list[folder] = num_files
+    
+    # Creating Test-list
+    for folder in folders[num_train:]:
+        num_files = len(os.listdir(os.path.join(directory, folder)))
+        test_list[folder] = num_files  
+    
+    return train_list, test_list
+
+def create_triplets(directory, folder_list, max_files=10):
+    triplets = []
+    folders = list(folder_list.keys())
+    
+    for folder in folders:
+        path = os.path.join(directory, folder)
+        files = list(os.listdir(path))[:max_files]
+        num_files = len(files)
+        
+        for i in range(num_files-1):
+            for j in range(i+1, num_files):
+                anchor = (folder, f"{i}.jpg")
+                positive = (folder, f"{j}.jpg")
+
+                neg_folder = folder
+                while neg_folder == folder:
+                    neg_folder = random.choice(folders)
+                neg_file = random.randint(0, folder_list[neg_folder]-1)
+                negative = (neg_folder, f"{neg_file}.jpg")
+
+                triplets.append((anchor, positive, negative))
+            
+    random.shuffle(triplets)
+    return triplets
+
+def get_batch(triplet_list, batch_size=256, preprocess=True):
+    batch_steps = len(triplet_list)//batch_size
+    
+    for i in range(batch_steps+1):
+        anchor   = []
+        positive = []
+        negative = []
+        
+        j = i*batch_size
+        while j<(i+1)*batch_size and j<len(triplet_list):
+            a, p, n = triplet_list[j]
+            anchor.append(read_image(a))
+            positive.append(read_image(p))
+            negative.append(read_image(n))
+            j+=1
+            
+        anchor = np.array(anchor)
+        positive = np.array(positive)
+        negative = np.array(negative)
+        
+        if preprocess:
+            anchor = preprocess_input(anchor)
+            positive = preprocess_input(positive)
+            negative = preprocess_input(negative)
+        
+        yield ([anchor, positive, negative])
+
+def get_encoder(input_shape):
+    """ Returns the image encoding model """
+
+    pretrained_model = Xception(
+        input_shape=input_shape,
+        weights='imagenet',
+        include_top=False,
+        pooling='avg',
+    )
+    
+    for i in range(len(pretrained_model.layers)-27):
+        pretrained_model.layers[i].trainable = False
+
+    encode_model = Sequential([
+        pretrained_model,
+        layers.Flatten(),
+        layers.Dense(512, activation='relu'),
+        layers.BatchNormalization(),
+        layers.Dense(256, activation="relu"),
+        layers.Lambda(lambda x: tf.math.l2_normalize(x, axis=1))
+    ], name="Encode_Model")
+    return encode_model
+
+class DistanceLayer(layers.Layer):
+    # A layer to compute ‖f(A) - f(P)‖² and ‖f(A) - f(N)‖²
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+
+    def call(self, anchor, positive, negative):
+        ap_distance = tf.reduce_sum(tf.square(anchor - positive), -1)
+        an_distance = tf.reduce_sum(tf.square(anchor - negative), -1)
+        return (ap_distance, an_distance)
+    
+
+def get_siamese_network(input_shape = (128, 128, 3)):
+    encoder = get_encoder(input_shape)
+    
+    # Input Layers for the images
+    anchor_input   = layers.Input(input_shape, name="Anchor_Input")
+    positive_input = layers.Input(input_shape, name="Positive_Input")
+    negative_input = layers.Input(input_shape, name="Negative_Input")
+    
+    ## Generate the encodings (feature vectors) for the images
+    encoded_a = encoder(anchor_input)
+    encoded_p = encoder(positive_input)
+    encoded_n = encoder(negative_input)
+    
+    # A layer to compute ‖f(A) - f(P)‖² and ‖f(A) - f(N)‖²
+    distances = DistanceLayer()(
+        encoder(anchor_input),
+        encoder(positive_input),
+        encoder(negative_input)
+    )
+    
+    # Creating the Model
+    siamese_network = Model(
+        inputs  = [anchor_input, positive_input, negative_input],
+        outputs = distances,
+        name = "Siamese_Network"
+    )
+    return siamese_network
+
+class SiameseModel(Model):
+    # Builds a Siamese model based on a base-model
+    def __init__(self, siamese_network, margin=1.0):
+        super(SiameseModel, self).__init__()
+        
+        self.margin = margin
+        self.siamese_network = siamese_network
+        self.loss_tracker = metrics.Mean(name="loss")
+
+    def call(self, inputs):
+        return self.siamese_network(inputs)
+
+    def train_step(self, data):
+        # GradientTape get the gradients when we compute loss, and uses them to update the weights
+        with tf.GradientTape() as tape:
+            loss = self._compute_loss(data)
+            
+        gradients = tape.gradient(loss, self.siamese_network.trainable_weights)
+        self.optimizer.apply_gradients(zip(gradients, self.siamese_network.trainable_weights))
+        
+        self.loss_tracker.update_state(loss)
+        return {"loss": self.loss_tracker.result()}
+
+    def test_step(self, data):
+        loss = self._compute_loss(data)
+        
+        self.loss_tracker.update_state(loss)
+        return {"loss": self.loss_tracker.result()}
+
+    def _compute_loss(self, data):
+        # Get the two distances from the network, then compute the triplet loss
+        ap_distance, an_distance = self.siamese_network(data)
+        loss = tf.maximum(ap_distance - an_distance + self.margin, 0.0)
+        return loss
+
+    @property
+    def metrics(self):
+        # We need to list our metrics so the reset_states() can be called automatically.
+        return [self.loss_tracker]
+
+def test_on_triplets(batch_size = 256, test_triplet = None, siamese_model = None):
+    pos_scores, neg_scores = [], []
+
+    for data in get_batch(test_triplet, batch_size=batch_size):
+        prediction = siamese_model.predict(data)
+        pos_scores += list(prediction[0])
+        neg_scores += list(prediction[1])
+    
+    accuracy = np.sum(np.array(pos_scores) < np.array(neg_scores)) / len(pos_scores)
+    ap_mean = np.mean(pos_scores)
+    an_mean = np.mean(neg_scores)
+    ap_stds = np.std(pos_scores)
+    an_stds = np.std(neg_scores)
+    
+    print(f"Accuracy on test = {accuracy:.5f}")
+    return (accuracy, ap_mean, an_mean, ap_stds, an_stds)
+
+"""
+def plot_metrics(loss, metrics):
+    # Extracting individual metrics from metrics
+    accuracy = metrics[:, 0]
+    ap_mean  = metrics[:, 1]
+    an_mean  = metrics[:, 2]
+    ap_stds  = metrics[:, 3]
+    an_stds  = metrics[:, 4]
+    
+    plt.figure(figsize=(15,5))
+    
+    # Plotting the loss over epochs
+    plt.subplot(121)
+    plt.plot(loss, 'b', label='Loss')
+    plt.title('Training loss')
+    plt.legend()
+    
+    # Plotting the accuracy over epochs
+    plt.subplot(122)
+    plt.plot(accuracy, 'r', label='Accuracy')
+    plt.title('Testing Accuracy')
+    plt.legend()
+    
+    plt.figure(figsize=(15,5))
+    
+    # Comparing the Means over epochs
+    plt.subplot(121)
+    plt.plot(ap_mean, 'b', label='AP Mean')
+    plt.plot(an_mean, 'g', label='AN Mean')
+    plt.title('Means Comparision')
+    plt.legend()
+    
+    # Plotting the accuracy
+    ap_75quartile = (ap_mean+ap_stds)
+    an_75quartile = (an_mean-an_stds)
+    plt.subplot(122)
+    plt.plot(ap_75quartile, 'b', label='AP (Mean+SD)')
+    plt.plot(an_75quartile, 'g', label='AN (Mean-SD)')
+    plt.title('75th Quartile Comparision')
+    plt.legend()
+"""
+
+def extract_encoder(model):
+    encoder = get_encoder((128, 128, 3))
+    i=0
+    for e_layer in model.layers[0].layers[3].layers:
+        layer_weight = e_layer.get_weights()
+        encoder.layers[i].set_weights(layer_weight)
+        i+=1
+    return encoder
+
+def classify_images(face_list1, face_list2, threshold=1.3, encoder = None):
+    # Getting the encodings for the passed faces
+    tensor1 = encoder.predict(face_list1)
+    tensor2 = encoder.predict(face_list2)
+    
+    distance = np.sum(np.square(tensor1-tensor2), axis=-1)
+    prediction = np.where(distance<=threshold, 0, 1)
+    return prediction
+
+"""
+def ModelMetrics(pos_list, neg_list):
+    true = np.array([0]*len(pos_list)+[1]*len(neg_list))
+    pred = np.append(pos_list, neg_list)
+    
+    # Compute and print the accuracy
+    print(f"\nAccuracy of model: {accuracy_score(true, pred)}\n")
+    
+    # Compute and plot the Confusion matrix
+    cf_matrix = confusion_matrix(true, pred)
+
+    categories  = ['Similar','Different']
+    names = ['True Similar','False Similar', 'False Different','True Different']
+    percentages = ['{0:.2%}'.format(value) for value in cf_matrix.flatten() / np.sum(cf_matrix)]
+
+    labels = [f'{v1}\n{v2}' for v1, v2 in zip(names, percentages)]
+    labels = np.asarray(labels).reshape(2,2)
+
+    sns.heatmap(cf_matrix, annot = labels, cmap = 'Blues',fmt = '',
+                xticklabels = categories, yticklabels = categories)
+
+    plt.xlabel("Predicted", fontdict = {'size':14}, labelpad = 10)
+    plt.ylabel("Actual"   , fontdict = {'size':14}, labelpad = 10)
+    plt.title ("Confusion Matrix", fontdict = {'size':18}, pad = 20)
+"""
